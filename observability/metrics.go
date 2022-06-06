d0bdfaecbe49cf2c29eabf359bd0e10ed0666566
Gabor Javorszky <gabor@suborbital.dev>
2022-06-06 17:26:39 +0100

    Add tracing and metrics implementations

--- observability/metrics.go
+++ observability/metrics.go
@@ -1,1 +1,51 @@
 package observability
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/metric/global"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	"google.golang.org/grpc"
+)
+
+// OtelMeter takes a grpc connection to an otel collector, a collectPeriod time.Duration (more than a second), and
+// sets the meter collector up and assigns it to a global meter. If the function returns nil, assume that the global
+// meter is now set and ready to be used.
+//
+// This function merely sets up the scaffolding to ship collected metered data to the opentelemetry collector. It does
+// not set up the specific meters for the applications.
+func OtelMeter(ctx context.Context, conn *grpc.ClientConn, collectPeriod time.Duration) error {
+	if collectPeriod < time.Second {
+		return errors.New("collect period is shorter than a second, please choose a longer period to avoid" +
+			" overloading the collector")
+	}
+
+	exporter, err := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithGRPCConn(conn),
+	)
+	if err != nil {
+		return errors.Wrap(err, "otlpmetricgrpc.New")
+	}
+
+	cont := controller.New(
+		processor.NewFactory(
+			simple.NewWithInexpensiveDistribution(),
+			exporter,
+		),
+		controller.WithExporter(exporter),
+		controller.WithCollectPeriod(collectPeriod),
+	)
+
+	if err := cont.Start(context.Background()); err != nil {
+		return errors.Wrap(err, "metric controller Start")
+	}
+
+	global.SetMeterProvider(cont)
+	return nil
+}
--- observability/tracing.go
+++ observability/tracing.go
@@ -1,1 +1,115 @@
 package observability
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const (
+	collectorExporterName = "collector"
+	honeycombExporterName = "honeycomb"
+)
+
+// TracingConfig is the minimum configuration needed to configure any of the tracing solutions that aren't the no-op
+// tracer.
+type TracingConfig struct {
+	Probability float64
+	ServiceName string
+}
+
+// HoneycombTracingConfig embeds the TracingConfig struct, and adds other, specifically Honeycomb related fields.
+type HoneycombTracingConfig struct {
+	TracingConfig
+	Endpoint string
+	APIKey   string
+	Dataset  string
+}
+
+// OtelTracer sets up a trace provider that sends data to an opentelemetry collector.
+func OtelTracer(ctx context.Context, conn *grpc.ClientConn, config TracingConfig) (*trace.TracerProvider, error) {
+	exporter, err := otlptrace.New(ctx, otlptracegrpc.NewClient(
+		otlptracegrpc.WithGRPCConn(conn),
+	))
+	if err != nil {
+		return nil, errors.Wrap(err, "oltptrace.New with exporter as collector")
+	}
+
+	traceOpts := tracerOpts(exporter, config.ServiceName, collectorExporterName, config.Probability)
+
+	traceProvider := trace.NewTracerProvider(traceOpts...)
+
+	otel.SetTracerProvider(traceProvider)
+
+	return traceProvider, nil
+}
+
+// HoneycombTracer returns a tracer provider configured to send traces to your Honeycomb account.
+func HoneycombTracer(ctx context.Context, config HoneycombTracingConfig) (*trace.TracerProvider, error) {
+	exporter, err := otlptrace.New(ctx, otlptracegrpc.NewClient(
+		otlptracegrpc.WithEndpoint(config.Endpoint),
+		otlptracegrpc.WithHeaders(map[string]string{
+			"x-honeycomb-team":    config.APIKey,
+			"x-honeycomb-dataset": config.Dataset,
+		}),
+		otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
+	))
+	if err != nil {
+		return nil, errors.Wrap(err, "oltptrace.New with exporter as honeycomb")
+	}
+
+	traceOpts := tracerOpts(exporter, config.ServiceName, honeycombExporterName, config.Probability)
+	traceProvider := trace.NewTracerProvider(traceOpts...)
+
+	otel.SetTracerProvider(traceProvider)
+
+	return traceProvider, nil
+
+}
+
+// NoopTracer returns a non-configured empty trace provider that won't do anything.
+func NoopTracer() (*trace.TracerProvider, error) {
+	// Create the most default trace provider and escape early.
+	traceProvider := trace.NewTracerProvider()
+
+	otel.SetTracerProvider(traceProvider)
+
+	return traceProvider, nil
+}
+
+// tracerOpts is a utility function to cut down on code duplication, as the tracer provider options overlap between the
+// collector and honeycomb tracer implementations.
+func tracerOpts(exporter trace.SpanExporter, serviceName, exporterName string, probability float64) []trace.TracerProviderOption {
+	return []trace.TracerProviderOption{
+		trace.WithSampler(trace.TraceIDRatioBased(probability)),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(serviceName),
+				attribute.String("exporter", exporterName),
+			),
+		),
+		trace.WithBatcher(exporter,
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+			trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+		),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(serviceName),
+				attribute.String("exporter", "honeycomb"),
+			),
+		),
+	}
+}
