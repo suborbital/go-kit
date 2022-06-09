6022b28b00ed5da49c2357c202e8f7d231863fe5
Gabor Javorszky <gabor@suborbital.dev>
2022-06-09 13:35:39 +0100

    Add timer

---
+++ observability/timer.go
@@ -0,0 +1,22 @@
+package observability
+
+import (
+	"time"
+)
+
+type timer struct {
+	start time.Time
+}
+
+// NewTimer returns a pointer to a timer instance with the start time set as the creation date.
+func NewTimer() *timer {
+	return &timer{
+		start: time.Now(),
+	}
+}
+
+// Observe returns the number of milliseconds between when the timer was constructed, and when this function is called
+// as an int64.
+func (t *timer) Observe() int64 {
+	return time.Now().Sub(t.start).Milliseconds()
+}
